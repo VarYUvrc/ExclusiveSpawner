e978f375c4b861262d73a45816d3be918069a420
sack-kazu <sack.kazu@gmail.com>
2023-07-02 11:44:12 +0900

    同期配列を使用して順番に部屋を使えるように

--- ExclusiveSpawner.cs
+++ ExclusiveSpawner.cs
@@ -1,4 +1,3 @@
-
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -6,43 +5,86 @@ using VRC.Udon;
 
 namespace Varyu.ExclusiveSpawner
 {
-    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-    public class ExclusiveSpawner : UdonSharpBehaviour
-    {
-        [SerializeField] private Transform VRCWorldSpawn;
-        [SerializeField] private Transform[] SpawnPoints;
-        [SerializeField] private GameObject[] EnableObjects;
+      [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+      public class ExclusiveSpawner : UdonSharpBehaviour
+      {
+            [SerializeField] private Transform VRCWorldSpawn;
+            [SerializeField] private Transform[] SpawnPoints;
+            [SerializeField] private GameObject[] EnableObjects;
 
-        public override void OnPlayerJoined(VRCPlayerApi player)
-        {
-            // 必要なかったら終わり
-            if (!Utilities.IsValid(player)) return;
-            if (!player.isLocal) return;
+            [UdonSynced] public int[] RoomUserArray;
+            private int localPlayerId = -1;
+            private int delayFrame = -1;
+            private VRCPlayerApi _localPlayer;
 
-            // プレイヤーIDを取得
-            int playerID = Networking.LocalPlayer.playerId;
-
-            // プレイヤーIDが0または17以上の場合、IDを8から16の範囲にランダムに設定
-            if (playerID < 1 || playerID > 17)
+            void Start()
             {
-                playerID = Random.Range(8, 16);
+                  _localPlayer = Networking.LocalPlayer;
+                  if (_localPlayer == null) return;
+
+                  RoomUserArray = new int[SpawnPoints.Length];
+                  for (int i = 0; i < RoomUserArray.Length; i++)
+                  {
+                        RoomUserArray[i] = -1;
+                  }
+
+                  localPlayerId = _localPlayer.playerId;
             }
 
-            // スポーンポイントIndexをプレイヤーIDから計算（0-16の範囲にするために-1する）
-            int spawnPointIndex = playerID - 1;
-
-            // スポーンポイント変更
-            VRCWorldSpawn.position = SpawnPoints[spawnPointIndex].position;
-            VRCWorldSpawn.rotation = SpawnPoints[spawnPointIndex].rotation;
-
-            // そこにワープ
-            player.TeleportTo(VRCWorldSpawn.position, VRCWorldSpawn.rotation);
-
-            // EnableObjectsの処理
-            for (int i = 0; i < EnableObjects.Length; i++)
+            public void AssignRoom()
             {
-                EnableObjects[i].SetActive(i == spawnPointIndex);
+                  for (int i = 0; i < RoomUserArray.Length; i++)
+                  {
+                        Networking.SetOwner(_localPlayer, gameObject);
+                        RequestSerialization();
+                        if (RoomUserArray[i] == -1)
+                        {
+                              RoomUserArray[i] = localPlayerId;
+                              TeleportSequence(i);
+                              break;
+                        }
+                  }
             }
-        }
-    }
+
+            private void TeleportSequence(int roomNumber)
+            {
+                  // ワールドスポーンを移動
+                  VRCWorldSpawn.position = SpawnPoints[roomNumber].position;
+                  VRCWorldSpawn.rotation = SpawnPoints[roomNumber].rotation;
+
+                  // そこにワープ
+                  _localPlayer.TeleportTo(VRCWorldSpawn.position, VRCWorldSpawn.rotation);
+
+                  // EnableObjectsの処理
+                  for (int i = 0; i < EnableObjects.Length; i++)
+                  {
+                        EnableObjects[i].SetActive(i == roomNumber);
+                  }
+            }
+
+            public override void OnPlayerJoined(VRCPlayerApi player)
+            {
+                  // 必要なかったら終わり
+                  if (!Utilities.IsValid(player)) return;
+                  if (!player.isLocal) return;
+
+                  // 部屋の割り当てを遅延実行
+                  // 同時にJoinした人がいた場合の安全策
+                  delayFrame = localPlayerId % RoomUserArray.Length;
+                  SendCustomEventDelayedFrames(nameof(AssignRoom), delayFrame * 3);
+            }
+            public override void OnPlayerLeft(VRCPlayerApi player)
+            {
+                  if (!Networking.IsOwner(_localPlayer, gameObject)) return;
+
+                  for (int i = 0; i < RoomUserArray.Length; i++)
+                  {
+                        if (RoomUserArray[i] == player.playerId)
+                        {
+                              RoomUserArray[i] = -1;
+                              break;
+                        }
+                  }
+            }
+      }
 }
