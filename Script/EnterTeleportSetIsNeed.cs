b07486ca7458bc54a6d30df07399db21c16a1bb7
VarYU <varyu.vrc@gmail.com>
2024-03-03 16:43:29 +0900

    🔨 クラスの更新に合わせてサンプルシーン更新

---
+++ Script/EnterTeleportSetIsNeed.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Varyu.ExclusiveSpawner
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class EnterTeleportSetIsNeed : UdonSharpBehaviour
+    {
+        [SerializeField] GameObject[] HideObject;
+        [SerializeField] GameObject ShowObject;
+        [SerializeField] Transform Destination;
+        [SerializeField] private ExclusiveSpawnerV2 ExclusiveSpawner;
+        int _localPlayerID;
+
+        void Start()
+        {
+            _localPlayerID = Networking.LocalPlayer.playerId;
+        }
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if(!player.isLocal) return;
+            ShowObject.SetActive(true);
+            foreach (GameObject obj in HideObject)
+            {
+                obj.SetActive(false);
+            }
+            if(player.playerId == _localPlayerID)player.TeleportTo(Destination.position,Destination.rotation);
+            ExclusiveSpawner.SetIsNeedTeleport(false);
+        }
+    }
+}
